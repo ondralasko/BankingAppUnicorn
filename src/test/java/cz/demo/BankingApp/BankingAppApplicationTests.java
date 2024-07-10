c1d15d0af490b506d9b6572cb4d91958554ce50a
ondralasko <ondralasko@gmail.com>
2024-07-10 19:21:23 +0200

    Initial commit

---
+++ src/test/java/cz/demo/BankingApp/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.demo.BankingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
